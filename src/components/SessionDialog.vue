a2fe3f0eb0a548e07aaa94a34621c0ae94c447e1
macdfree <macdfree@163.com>
2025-05-12 22:59:58 +0800

    feat(components): 优化连接信息对话框展示效果
    - 重构连接信息展示逻辑，按分组进行展示
    - 优化对话框样式，增加默认展开所有节点功能
    - 调整对话框尺寸和位置，提升用户体验

--- src/components/SessionDialog.vue
+++ src/components/SessionDialog.vue
@@ -1,17 +1,3 @@
-<template>
-  <n-modal :show="showModal" mask-closable :on-mask-click="closeDialog">
-    <div class="modal-body" style="width: 400px; position: fixed; top: 20px; left: 50%; transform: translateX(-50%)">
-      <n-tree
-        block-line
-        expand-on-click
-        :data="connections"
-        :node-props="nodeProps"
-        :on-update:expanded-keys="updatePrefixWithExpaned"
-      />
-    </div>
-  </n-modal>
-</template>
-
 <script setup>
 import { Folder, FolderOpenOutline, DesktopOutline } from '@vicons/ionicons5';
 import { ref, h } from 'vue';
@@ -21,50 +7,72 @@ const emit = defineEmits(['login']);
 
 const showModal = ref(false);
 
-function openConnInfoDialog() {
-  window.configAPI.getAllConnConfigs().then((res) => {
-    console.log(res);
-    res.forEach((conn) => {
-      conn.key = conn.id;
-      conn.label = conn.name;
-      conn.prefix = () =>
-        h(NIcon, null, {
-          default: () => h(DesktopOutline),
-        });
-      conn.suffix = () =>
-        h(NButtonGroup, null, {
-          default: () => [
-            h(
-              NButton,
-              {
-                text: false,
-                type: 'default',
-                size: 'small',
-                onClick: () => loginSSH(event, conn.id, conn.name),
-              },
-              {
-                default: () => 'SSH',
-              },
-            ),
-            h(
-              NButton,
-              {
-                text: false,
-                type: 'default',
-                size: 'small',
-                onClick: () => loginSFTP(event, conn.id, conn.name),
-              },
-              {
-                default: () => 'SFTP',
-              },
-            ),
-          ],
-        });
-      // conn.children = null;
-    });
-    connections.value = res;
-  });
+const buildConnectionTree = async () => {
+  let res = await window.configAPI.getAllConnConfigs();
 
+  const grouped = res.reduce((acc, conn) => {
+    const groupName = conn.group || '未分组';
+    if (!acc[groupName]) {
+      acc[groupName] = [];
+    }
+    acc[groupName].push(conn);
+    return acc;
+  }, {});
+
+  connections.value = Object.entries(grouped).map(([groupName, conns]) => {
+    return {
+      label: groupName,
+      key: groupName,
+      prefix: () =>
+        h(NIcon, null, {
+          default: () => h(FolderOpenOutline),
+        }),
+      children: conns.map((conn) => {
+        return {
+          ...conn,
+          label: conn.name,
+          key: conn.id,
+          prefix: () =>
+            h(NIcon, null, {
+              default: () => h(DesktopOutline),
+            }),
+          suffix: () =>
+            h(NButtonGroup, null, {
+              default: () => [
+                h(
+                  NButton,
+                  {
+                    text: false,
+                    type: 'default',
+                    size: 'small',
+                    onClick: () => loginSSH(event, conn.id, conn.name),
+                  },
+                  {
+                    default: () => 'SSH',
+                  },
+                ),
+                h(
+                  NButton,
+                  {
+                    text: false,
+                    type: 'default',
+                    size: 'small',
+                    onClick: () => loginSFTP(event, conn.id, conn.name),
+                  },
+                  {
+                    default: () => 'SFTP',
+                  },
+                ),
+              ],
+            }),
+        };
+      }),
+    };
+  });
+};
+
+async function openConnInfoDialog() {
+  await buildConnectionTree();
   showModal.value = true;
 }
 
@@ -125,11 +133,31 @@ function loginSFTP(event, connectionID, title) {
 }
 </script>
 
+<template>
+  <n-modal :show="showModal" mask-closable :on-mask-click="closeDialog">
+    <div class="modal-body">
+      <n-tree
+        block-line
+        expand-on-click
+        default-expand-all
+        :data="connections"
+        :node-props="nodeProps"
+        :on-update:expanded-keys="updatePrefixWithExpaned"
+      />
+    </div>
+  </n-modal>
+</template>
+
 <style lang="less" scoped>
 .modal-body {
   background-color: #fff;
   border-radius: 4px;
-  padding: 5px 10px;
+  padding: 10px 10px;
   max-height: 350px;
+  width: 500px;
+  position: fixed;
+  top: 20px;
+  left: 50%;
+  transform: translateX(-50%);
 }
 </style>
