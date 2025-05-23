122b8969b72cff4ca88c0bc3f7b6bf4724adfd3f
macdfree <macdfree@163.com>
2025-05-23 23:01:13 +0800

    style(components): 优化设置组件图标导入和排版
    - 移除了未使用的 CreateOutline 组件
    - 调整了图标组件的导入列表
    - 优化了标签页图标和文本的排版，移除了多余的空格和换行

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { NIcon, NTabs, NTabPane } from 'naive-ui';
-import { Link, CreateOutline, Create, ReaderOutline, InformationCircleOutline } from '@vicons/ionicons5';
+import { Link, ReaderOutline, InformationCircleOutline } from '@vicons/ionicons5';
 import ConnectionSetting from './ConnectionSetting.vue';
 import EditorSetting from './EditorSetting.vue';
 import ConfigPathSetting from './ConfigPathSetting.vue';
@@ -11,25 +11,19 @@ import ConfigPathSetting from './ConfigPathSetting.vue';
     <n-tab-pane name="connection-setting">
       <ConnectionSetting />
       <template #tab>
-        <n-icon size="20">
-          <Link /> </n-icon
-        >&nbsp;&nbsp;连接配置
+        <n-icon size="20"> <Link /> </n-icon>&nbsp;&nbsp;连接配置
       </template>
     </n-tab-pane>
     <n-tab-pane name="editor-setting">
       <EditorSetting />
       <template #tab>
-        <n-icon size="20">
-          <ReaderOutline /> </n-icon
-        >&nbsp;&nbsp;编辑器配置
+        <n-icon size="20"> <ReaderOutline /> </n-icon>&nbsp;&nbsp;编辑器配置
       </template>
     </n-tab-pane>
     <n-tab-pane name="config-path-setting">
       <ConfigPathSetting />
       <template #tab>
-        <n-icon size="20">
-          <InformationCircleOutline /> </n-icon
-        >&nbsp;&nbsp;配置文件路径
+        <n-icon size="20"> <InformationCircleOutline /> </n-icon>&nbsp;&nbsp;配置文件路径
       </template>
     </n-tab-pane>
   </n-tabs>
